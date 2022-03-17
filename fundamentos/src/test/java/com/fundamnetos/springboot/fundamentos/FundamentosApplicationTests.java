ec5db3d046b2a984f2bd2dd0c4fc6151cbc1426d
Luisa <luisa.ramirezmo@amigo.edu.co>
2022-03-17 08:33:29 -0500

    Firt commit

---
+++ fundamentos/src/test/java/com/fundamnetos/springboot/fundamentos/FundamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamnetos.springboot.fundamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootT
+
+@SpringBootTest
+class FundamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
